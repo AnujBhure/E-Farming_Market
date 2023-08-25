e7fdbf1ce2d8cef4d01efdaf8395051ef69c21d2
pratik wanare <kharanshuw@gmail.com>
2023-08-26 00:57:10 +0530

    addeed admin using ado.net

---
+++ Back-end/src/main/Dot_Net/Efarming_2 using ado_dot_net/Model/user.cs
@@ -0,0 +1,19 @@
+﻿namespace Efarming_2.Model
+{
+    public class user
+    {
+        public int Uid { get; set; }
+
+        public string Fname { get; set; } = null!;
+
+        public string Lname { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Contact { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+    }
+}
